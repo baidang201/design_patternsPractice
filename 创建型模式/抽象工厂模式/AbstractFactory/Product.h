fe2c743d78150d8cf35135c4340332730cb71f24
liyh <jinrishouji@sina.com>
2017-01-03 16:48:07 +0800

    增加抽象工厂

---
+++ 创建型模式/抽象工厂模式/AbstractFactory/Product.h
@@ -0,0 +1,55 @@
+#ifndef __PRODUCT__H__
+#define __PRODUCT__H__
+
+class AbstractProductA
+{
+public:
+    virtual ~AbstractProductA();
+
+protected:
+    AbstractProductA();
+
+};
+
+
+class AbstractProductB
+{
+public:
+    virtual ~AbstractProductB();
+
+protected:
+    AbstractProductB();
+};
+
+
+class ProductA1 :public AbstractProductA
+{
+public :
+    ProductA1();
+    ~ProductA1();
+
+};
+class ProductA2 :public AbstractProductA
+{
+public:
+    ProductA2();
+    ~ProductA2();
+
+};
+
+class ProductB1 :public AbstractProductB
+{
+public:
+    ProductB1();
+    ~ProductB1();
+};
+
+class ProductB2 :public AbstractProductB
+{
+public:
+    ProductB2();    
+    ~ProductB2();
+
+};
+
+#endif
